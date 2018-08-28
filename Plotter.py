533bbcbce643f100ca852f7ca6caba675ee76b77
Dan.Ko <daniel.j.ko@gmail.com>
2018-08-28 15:59:51 +1200

    Finally plotting predicted prices. Looks not actually right

--- Plotter.py
+++ Plotter.py
@@ -1,17 +1,66 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 import Preprocess
 import DataReader
 
+
 sns.set()
 sns.set_style("whitegrid")
-sns.set(color_codes=True)
+
+sns.despine()
 
 df = DataReader.create_combined_df()
-pipeline = Preprocess.process()
+pipeline = Preprocess.process(df)
 regressor = pipeline.named_steps["regressor"]
 # print(regressor.mse_path_)
 
-# sns.lmplot(x="Year", y=df.columns.values, data=df.values, fit_reg=True)
+
+# Score
+predicted = pipeline.fit(df, df["Wellington"]).predict(df)
+score = pipeline.score(df, df["Wellington"])
+print(f"SCORE: {score}")
+
+# Plot significant features
+feat_importances = pipeline.named_steps["feat_select"].estimator_.feature_importances_
+
+feat_support = pipeline.named_steps["feat_select"].get_support()
+
+sig_feats = pd.DataFrame(
+    feat_support.reshape(-1, len(feat_support)),
+    index=["Important feature?"],
+    columns=df.columns,
+)
+sig_feats.loc["Importance score"] = feat_importances
+
+# sig_feats.iloc[1].plot.bar(
+#     legend=False,
+#     y="Importance",
+#     x="Feature",
+#     title="Feature importance",
+#     use_index=True,
+#     stacked=False,
+# )
+
+# Plot normalised features
+normalised = pipeline.named_steps["standardize"]
+
+scaled_data = StandardScaler().fit_transform(df)
+df_scaled = pd.DataFrame(scaled_data, index=df.index, columns=df.columns)
+
+# df_scaled.plot(legend=False)
+
+predicted_prices = pd.DataFrame(predicted, columns=["Predicted Yearly Rent Price"])
+predicted_prices.loc[:, "Year"] = df.index  # df[(df.index > "2007-12-31")]
+
+sns.scatterplot(x="Year", y="Predicted Yearly Rent Price", data=predicted_prices)
+
+for price in predicted_prices.loc[:, "Predicted Yearly Rent Price"]:
+    print(price / 12)
+# plt.scatter(df.values, df["Wellington"].values, color="black")
+# plt.plot(df.index, predicted, color="red", linewidth=3)
+plt.show()
+
+# sns.lmplot(x="Year", y=df.columns.values, data=df.values, fit_reg=)
--- Preprocess.py
+++ Preprocess.py
@@ -21,24 +21,11 @@ from Applier import Applier
 KFOLDS = 10
 
 
-def process():
-    df = DataReader.create_combined_df()
-
+def process(df):
     pipeline = Pipeline(
         memory=None,
         steps=[
-            (
-                "feat_union",
-                FeatureUnion(
-                    n_jobs=1,
-                    transformer_list=[
-                        (
-                            "normalised",
-                            Pipeline([("scaler", StandardScaler())]),
-                        )  # ("apply_df", Applier(np.number))
-                    ],
-                ),
-            ),
+            ("standardize", StandardScaler()),
             (
                 "feat_select",
                 ftselect.SelectFromModel(RandomForestRegressor())
@@ -50,25 +37,6 @@ def process():
             ),  # AdaBoostRegressor()
         ],
     )
-    predicted = pipeline.fit(df, df["Wellington"]).predict(df)
-    score = pipeline.score(df, df["Wellington"])
-    print(f"SCORE: {score}")
-
-    feat_importances = pipeline.named_steps[
-        "feat_select"
-    ].estimator_.feature_importances_
-
-    feat_support = pipeline.named_steps["feat_select"].get_support()
-
-    sig_feats = pd.DataFrame(
-        feat_support.reshape(-1, len(feat_support)),
-        index=["Important feature?"],
-        columns=df.columns,
-    )
-    # for i, score in enumerate(feat_importances):
-    sig_feats.loc["Importance score"] = feat_importances
-    print(sig_feats)
-    # display(sig_feats)
 
     # scores = cvalid.cross_val_score(
     #     pipeline,
@@ -86,4 +54,4 @@ def process():
 
 
 if __name__ == "__main__":
-    process()
+    process(DataReader.create_combined_df())
